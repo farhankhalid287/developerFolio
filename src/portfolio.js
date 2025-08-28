f8e375a834d824fac978a49e598a129781dd1273
farhankhalid287 <farhan.khalid287@gmail.com>
2025-08-28 15:49:01 +0500

    Added College info

--- src/portfolio.js
+++ src/portfolio.js
@@ -131,7 +131,7 @@ const educationInfo = {
   schools: [
     {
       schoolName: "Foundation University Islamabad",
-      logo: require("./assets/images/harvardLogo.png"),
+      logo: require("./assets/images/fuiLogo.png"),
       subHeader: "Bachelor of Science in Computer Science",
       duration: "September 2012 - April 2016",
       desc: "Participated in the research of XXX and published 3 papers.",
@@ -139,15 +139,15 @@ const educationInfo = {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
       ]
-    }
-    /* {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
+    },
+    {
+      schoolName: "Fazaia Inter College Nur Khan Chaklala",
+      logo: require("./assets/images/fazaiaLogo.jpg"),
+      subHeader: "Intermediate in Computer Science",
+      duration: "April 2010 - March 2012",
       desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
       descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
-    } */
+    }
   ]
 };
 
