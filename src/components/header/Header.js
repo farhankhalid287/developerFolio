9f7e45029c8355da5f4c71ea558545836597eab4
farhankhalid287 <farhan.khalid287@gmail.com>
2025-12-04 16:19:10 +0500

    test

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React, {useContext} from "react";
 import Headroom from "react-headroom";
 import "./Header.scss";
-import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+//import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import StyleContext from "../../contexts/StyleContext";
 import {
   greeting,
@@ -93,12 +93,11 @@ function Header() {
           <li>
             <a href="#contact">Contact Me</a>
           </li>
-          <li>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          {/* <li>
             <a>
               <ToggleSwitch />
             </a>
-          </li>
+          </li> */}
         </ul>
       </header>
     </Headroom>
